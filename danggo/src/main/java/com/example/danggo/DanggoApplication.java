09badc584f6f6e05848792d8669409dccb49a146
yujin <dkdk506@naver.com>
2025-02-20 15:21:50 +0900

    Initial commit

---
+++ danggo/src/main/java/com/example/danggo/DanggoApplication.java
@@ -0,0 +1,13 @@
+package com.example.danggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanggoApplication.class, args);
+	}
+
+}
